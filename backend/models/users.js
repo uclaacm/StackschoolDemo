6f8d2953e9c65e611ae8fa720dfa35c25e3dab3a
jameswu <jamesw0462@gmail.com>
2022-12-24 17:20:16 -0800

    Created endpoints

--- backend/models/users.js
+++ backend/models/users.js
@@ -5,45 +5,44 @@ const Schema = mongoose.Schema;
 const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        // index: { unique: true }
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    // index: { unique: true }
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-UserSchema.pre(save, function(next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
 
-    // Generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // Generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-        // hash password using salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
+    // hash password using salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
 
-            // override cleartext password with hashed password
-            user.password = hash;
-            next();
-        });
+      // override cleartext password with hashed password
+      user.password = hash;
+      next();
     });
+  });
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
-
 
 // Followed authentication tutorial here: https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,96 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://nathanz:2eXoIF3xb5tMqEns@app.n7kcunu.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("Connected to DB")).catch(console.error);
+mongoose.set('strictQuery', false);
 
-app.listen(3001, () => console.log("Server listening on port 3001"));
+mongoose
+  .connect(
+    'mongodb+srv://jamesw:fEEq4PJO8brPiG7V@app.n7kcunu.mongodb.net/test',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connected to DB'))
+  .catch(console.error);
+
+app.listen(3001, () => console.log('Server listening on port 3001'));
+
+const User = require('./models/users');
+const Post = require('./models/post');
+
+//Users endpoints
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+app.post('/users/new', (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  user.save();
+
+  res.json(user);
+});
+
+app.delete('/users/delete/:_id', async (req, res) => {
+  const result = await User.findByIdAndDelete(req.params._id);
+
+  res.json(result);
+});
+
+app.put('/users/edit/:_id', async (req, res) => {
+  const user = await User.findById(req.params._id);
+
+  user.username = req.body.username;
+  user.password = req.body.password;
+  user.save();
+
+  res.json(user);
+});
+
+//Posts endpoints
+app.get('/feed', async (req, res) => {
+  const feed = await Post.find();
+
+  res.json(feed);
+});
+
+app.post('/feed/new', (req, res) => {
+  const post = new Post({
+    content: req.body.content,
+    user: req.body.user,
+  });
+
+  post.save();
+
+  res.json(post);
+});
+
+app.delete('/feed/delete/:_id', async (req, res) => {
+  const result = await Post.findByIdAndDelete(req.params._id);
+
+  res.json(result);
+});
+
+app.put('/feed/edit/:_id', async (req, res) => {
+  const post = await Post.findById(req.params._id);
+
+  post.content = req.body.content;
+  post.save();
+
+  res.json(post);
+});
+
+app.put('/feed/update/:_id', async (req, res) => {
+  const post = await Post.findById(req.params._id);
+
+  post.num_likes++;
+  post.save();
+
+  res.json(post);
+});
